3d95d557931a60c967d744c17eaa2c3e444d8b50
Abdullateef Ado <olatunbosunlateef6@gmail.com>
2022-09-10 06:48:18 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/ban
+ #include <stdio.h>
+/**
+ *  * main - main block
+ *   * Return: 0
+ *    */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}

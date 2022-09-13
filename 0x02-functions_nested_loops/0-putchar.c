6eafcaeb17f7635232c628c41e4c26ee27651dcb
Abdullateef Ado <olatunbosunlateef6@gmail.com>
2022-09-13 13:52:59 -0700

    task o

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+/**
+ * main - print the word _putchar
+ *
+ * Return: 0 on sucess execution.
+ */
+
+int main(void)
+{
+	printf(" _puchar \n");
+	return (0);
+}

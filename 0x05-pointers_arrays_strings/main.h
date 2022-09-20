9ea0e3129cd7b6aaa09b3116f4b5508a4a9cab16
Abdullateef Ado <olatunbosunlateef6@gmail.com>
2022-09-19 17:51:26 -0700

    Task 0.98 Battery .st

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 
 int _putchar(char c);
 void reset_to_98(int *n);
-void swap-int(int *a, int *b);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);

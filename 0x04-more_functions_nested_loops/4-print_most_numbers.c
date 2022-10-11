4f890d928d768a5a53e75059a199bfa11a611adc
Abdullateef Ado <olatunbosunlateef6@gmail.com>
2022-10-10 19:17:42 -0700

    Task 4 I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,19 +5,17 @@
  *
  * Description: Prints the numbers with _putchar
  *
- * Return: void
+ * Return:
  */
 
 void print_most_numbers(void)
 {
-	int m;
 
-	for (m = '0'; m <= '9'; m++)
-	{
-		if (m != '2' && m != '4')
-			
-		else
-			_putchar(m);
-	}
-	_putchar('\n');
+    int m;
+    for (m = '0'; m <= '9'; m++)
+    {
+        if (m != '2' && m != '4')
+            putchar(m);
+    }
+    putchar('\n');
 }

f337937f29f1209130af88fe1e81fe00c07c568d
Young-proflat <olatunbosunlateef6@gmail.com>
2023-07-13 10:03:14 -0700

    Header-file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,19 @@
+#ifndef "SEARCH_ALGOS.H"
+#define "SEARCH_ALGOS_H"
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * Main - Entry point
+ *
+ * Return: Always EXIT_SUCESS
+ */
+
+int linear_search(int *array, size_t size, int value);
+
+
+#endif /** SEARCH_ALGOS.H */
+
+
